6856a616d7530bf721d254adbc2fc02331e361ce
Marko Pavlovic <markuspavlovic98@gmail.com>
2025-09-20 17:54:37 +0200

    UI build

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const morgan=require('morgan')
 morgan.token('body', req=>JSON.stringify(req.body))
 
 const app=express()
+app.use(express.static('dist'))
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body', {
     skip: (req)=>req.method=='GET'
